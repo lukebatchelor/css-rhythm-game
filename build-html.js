3f0d49cef5095207911a6318783661f046800fc9
Luke.Batchelor <batchelor.luke+git@gmail.com>
2018-09-03 23:40:08 +1000

    Removes bottom link as it gets in the way of play

--- build-html.js
+++ build-html.js
@@ -73,7 +73,6 @@ const htmlTemplate = `
         </div>
       </div>
     </form>
-    <p>Check out <a href="https://github.com/lukebatchelor/css-rhythm-game">lukebatchelor/css-rhythm-game</a> if you're interested in how it works!</p>
   </div>
   <a href="https://github.com/lukebatchelor/css-rhythm-game"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
   </body>
